a70cc85ab7f7a4fddf7a48a79e7a12c4f853e361
Brandon Ivey <brandon.ivey@coxinc.com>
2013-04-18 15:42:10 -0400

    toxapalooza

--- marimo_comments/tests/settings.py
+++ marimo_comments/tests/settings.py
@@ -3,3 +3,17 @@ Test Configuration
 """
 SECRET_KEY = "desabamento-na-obra-da-arena-palestra-mata-uma-pessoa"
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'marimo_comments',
+)
